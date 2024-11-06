1616c468825b54900489b285b02d15f5052cc559
haphong463 <haphong463@gmail.com>
2024-11-07 02:29:15 +0700

    PAYLOAD: add request, response

---
+++ flight-service/src/main/java/com/windev/flight_service/payload/request/CreateFlightRequest.java
@@ -0,0 +1,43 @@
+package com.windev.flight_service.payload.request;
+
+import com.windev.flight_service.dto.SeatDTO;
+import jakarta.validation.constraints.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class CreateFlightRequest {
+
+    @NotBlank(message = "Flight number is required")
+    @Size(max = 10, message = "Flight number must not exceed 10 characters")
+    private String flightNumber;
+
+    @NotBlank(message = "Airline is required")
+    @Size(max = 100, message = "Airline must not exceed 100 characters")
+    private String airline;
+
+    @NotBlank(message = "Origin is required")
+    @Size(max = 100, message = "Origin must not exceed 100 characters")
+    private String origin;
+
+    @NotBlank(message = "Destination is required")
+    @Size(max = 100, message = "Destination must not exceed 100 characters")
+    private String destination;
+
+    @NotNull(message = "Departure time is required")
+    @Future(message = "Departure time must be in the future")
+    private Date departureTime;
+
+    @NotNull(message = "Arrival time is required")
+    @Future(message = "Arrival time must be in the future")
+    private Date arrivalTime;
+
+    @NotNull(message = "Number of Economy seats is required")
+    @Min(value = 1, message = "There must be at least one Economy seat")
+    private Integer economySeats;
+
+    @NotNull(message = "Number of Business seats is required")
+    @Min(value = 1, message = "There must be at least one Business seat")
+    private Integer businessSeats;
+}
---
+++ flight-service/src/main/java/com/windev/flight_service/payload/request/CreateSeatRequest.java
@@ -0,0 +1,18 @@
+package com.windev.flight_service.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateSeatRequest {
+    private String seatNumber;
+
+    private String type;
+
+    private Boolean isAvailable;
+
+    private Double price;
+}
