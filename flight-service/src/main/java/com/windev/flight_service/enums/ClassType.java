82fc5f5d83a0c095e9dc92e849539a2dc4cbf54d
haphong463 <haphong463@gmail.com>
2024-11-07 02:28:23 +0700

    ENUMS: add seat class, flight status

---
+++ flight-service/src/main/java/com/windev/flight_service/enums/ClassType.java
@@ -0,0 +1,5 @@
+package com.windev.flight_service.enums;
+
+public enum ClassType {
+    ECONOMY, BUSINESS, FIRST
+}
---
+++ flight-service/src/main/java/com/windev/flight_service/enums/FlightStatus.java
@@ -0,0 +1,5 @@
+package com.windev.flight_service.enums;
+
+public enum FlightStatus {
+    ON_TIME, DELAYED, CANCELLED
+}
