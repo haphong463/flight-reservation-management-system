c0406b378389ad69e33876bdc5da8056939ed9b9
haphong463 <haphong463@gmail.com>
2024-11-07 21:50:32 +0700

    MODEL: add user profile to show detail information

--- user-service/src/main/java/com/windev/user_service/model/UserProfile.java
+++ user-service/src/main/java/com/windev/user_service/model/UserProfile.java
@@ -1,44 +1,37 @@
 package com.windev.user_service.model;
 
 import java.util.Date;
-import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "users")
-@Builder
-public class User {
-
+@Document(collection = "user_profiles")
+public class UserProfile {
     @Id
     private String id;
 
-    private String username;
+    @Indexed(unique = true)
+    private String userId;
 
-    private String password;
+    private String firstName;
+    private String lastName;
+    private Address address;
 
-    private String email;
-
-    @DBRef
-    private List<Role> roles;
-
-    private Preferences preferences;
-
-    private boolean emailVerified;
+    private String phone;
+    private Date dob;
 
     @CreatedDate
     private Date createdAt;
 
     @LastModifiedDate
     private Date updatedAt;
-
 }
