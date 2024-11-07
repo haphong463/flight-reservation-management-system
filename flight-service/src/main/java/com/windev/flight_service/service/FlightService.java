1b33f8ae79842f14439d85e5e142601e7b77440e
haphong463 <haphong463@gmail.com>
2024-11-07 09:51:53 +0700

    SERVICE: feature: add update seat, search, ...

--- flight-service/src/main/java/com/windev/flight_service/service/FlightService.java
+++ flight-service/src/main/java/com/windev/flight_service/service/FlightService.java
@@ -2,19 +2,32 @@ package com.windev.flight_service.service;
 
 import com.windev.flight_service.dto.FlightDTO;
 
+import com.windev.flight_service.dto.FlightDetailDTO;
+import com.windev.flight_service.dto.SeatDTO;
 import com.windev.flight_service.payload.request.CreateFlightRequest;
 import com.windev.flight_service.payload.request.UpdateFlightRequest;
+import com.windev.flight_service.payload.request.UpdateSeatRequest;
 import com.windev.flight_service.payload.response.PaginatedResponse;
+import java.util.Date;
 import org.springframework.data.domain.Pageable;
 
 public interface FlightService {
-    PaginatedResponse<FlightDTO> getAllFlights(Pageable pageable);
 
-    FlightDTO getOneFlight(String id);
+    //                                  FLIGHT
+    PaginatedResponse<FlightDTO> getAllFlights(int pageNumber, int pageSize);
 
-    FlightDTO createFlight(CreateFlightRequest request);
+    FlightDetailDTO getOneFlight(String id);
 
-    FlightDTO updateFlight(UpdateFlightRequest flight);
+    FlightDetailDTO createFlight(CreateFlightRequest request);
+
+    FlightDetailDTO updateFlight(String id, UpdateFlightRequest flight);
 
     void deleteFlight(String id);
+
+    PaginatedResponse<FlightDTO> searchFlights(String origin, String destination, Date departureDate, int pageNumber,
+                                               int pageSize);
+
+    //                                  SEAT
+    SeatDTO updateSeat(String flightId, String seatId, UpdateSeatRequest request);
+
 }
