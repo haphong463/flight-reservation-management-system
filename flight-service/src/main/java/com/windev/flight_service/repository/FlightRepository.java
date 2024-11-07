88d4f6726f823632f3d9c304e92af5de4cd7bcaa
haphong463 <haphong463@gmail.com>
2024-11-07 09:53:46 +0700

    REPOSITORY: feature: add search flight

--- flight-service/src/main/java/com/windev/flight_service/repository/FlightRepository.java
+++ flight-service/src/main/java/com/windev/flight_service/repository/FlightRepository.java
@@ -1,6 +1,7 @@
 package com.windev.flight_service.repository;
 
 import com.windev.flight_service.entity.Flight;
+import java.util.Date;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FlightRepository extends JpaRepository<Flight, String> {
     @Override
     Page<Flight> findAll(Pageable pageable);
+
+    Page<Flight> findByOriginAndDestinationAndDepartureTimeBetween(String origin, String destination, Date start, Date end, Pageable pageable);
 }
