00a15ed362c9665e5c5332a35b543a1649612d59
haphong463 <haphong463@gmail.com>
2024-11-07 09:53:24 +0700

    MAPPER: add more property to map

--- flight-service/src/main/java/com/windev/flight_service/mapper/FlightMapper.java
+++ flight-service/src/main/java/com/windev/flight_service/mapper/FlightMapper.java
@@ -1,13 +1,24 @@
 package com.windev.flight_service.mapper;
 
 import com.windev.flight_service.dto.FlightDTO;
+import com.windev.flight_service.dto.FlightDetailDTO;
 import com.windev.flight_service.entity.Flight;
 import com.windev.flight_service.payload.request.CreateFlightRequest;
+import com.windev.flight_service.payload.request.UpdateFlightRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface FlightMapper {
     FlightDTO toDTO(Flight flight);
+
+    FlightDetailDTO toDetailDTO(Flight flight);
+
+    FlightDetailDTO fromFlightDTOtoDetail(FlightDTO flightDTO);
+
     void createFlightFromRequest(CreateFlightRequest request, @MappingTarget Flight flight);
+
+    void updateFlightFromRequest(UpdateFlightRequest request, @MappingTarget Flight flight);
+
 }
--- flight-service/src/main/java/com/windev/flight_service/mapper/SeatMapper.java
+++ flight-service/src/main/java/com/windev/flight_service/mapper/SeatMapper.java
@@ -1,7 +1,10 @@
 package com.windev.flight_service.mapper;
 
+import com.windev.flight_service.dto.SeatDTO;
+import com.windev.flight_service.entity.Seat;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface SeatMapper {
+    SeatDTO toDTO(Seat seat);
 }
