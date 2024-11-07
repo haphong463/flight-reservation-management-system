2e1aa9f61af9911546bd22a3b3bbb79a223cdbef
haphong463 <haphong463@gmail.com>
2024-11-07 09:47:18 +0700

    CONFIG: add redis config

---
+++ flight-service/src/main/java/com/windev/flight_service/config/RedisConfig.java
@@ -0,0 +1,53 @@
+package com.windev.flight_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${redis.port}")
+    private int REDIS_PORT;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setPort(REDIS_PORT);
+        configuration.setHostName(REDIS_HOST);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+        // Đặt serializers
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
