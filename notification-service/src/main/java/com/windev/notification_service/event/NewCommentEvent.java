5b20440348158fbd84370733dc5074b3301bfb05
haphong463 <haphong463@gmail.com>
2024-11-16 12:14:59 +0700

    'ok'

--- notification-service/src/main/java/com/windev/notification_service/event/NewCommentEvent.java
+++
@@ -1,9 +0,0 @@
-package com.windev.notification_service.event;
-
-import lombok.Data;
-
-@Data
-public class NewCommentEvent {
-    private Long articleId;
-    private String content;
-}
--- notification-service/src/main/java/com/windev/notification_service/handler/NotificationStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.windev.notification_service.handler;
-
-public interface NotificationStrategy {
-    void sendNotification(Object data);
-}
