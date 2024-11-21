7c46765b8da04dee011349facf1d39cddc23f394
haphong463 <haphong463@gmail.com>
2024-11-21 12:16:09 +0700

    ok

--- flight-service/src/main/java/com/windev/flight_service/service/DistributedLocker.java
+++ flight-service/src/main/java/com/windev/flight_service/service/DistributedLocker.java
@@ -2,19 +2,7 @@ package com.windev.flight_service.service;
 
 import java.util.concurrent.TimeUnit;
 
-public interface RedisDistributedLocker {
-
-    // Here, only write the interface for distributed Lock
-    // And please explain each function, in public.. read
-    boolean tryLock(long waitTime, long leaseTime, TimeUnit unit) throws InterruptedException;
-
-    void lock(long leaseTime, TimeUnit unit);
-
-    void unlock();
-
-    boolean isLocked();
-
-    boolean isHeldByThread(long threadId);
-
-    boolean isHeldByCurrentThread();
+public interface DistributedLocker {
+    boolean tryLock(String key, long waitTime, long leaseTime, TimeUnit timeUnit) throws InterruptedException;
+    void unlock(String key);
 }
